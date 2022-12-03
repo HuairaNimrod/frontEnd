0ab3a95828173d380062ff984f3f842c209c94f8
nimax <nimax29@gmail.com>
2022-12-02 23:35:21 -0700

    json

--- chamber/scripts/companies.js
+++ chamber/scripts/companies.js
@@ -7,33 +7,42 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);
-    const companies=jsonObject["companies"];
-    companies.forEach(displayCompanies);
-  });
 
-  function displayCompanies(companie){
+    const companies = jsonObject["companies"];
+    companies.forEach(displayCompanies);
+  })
+
+function displayCompanies(company){
     // Create elements to add to the document  
   let card = document.createElement("section");
-  let portrait = document.createElement("img");
-  let heading = document.createElement("h2");
-  let paragraph = document.createElement("p");
-  let paragraph2 = document.createElement("p");
-  let website = document.createElement("p");
-  let paragraph4 = document.createElement("p");
+  let image = document.createElement("img");
+  let h2 = document.createElement("h4");
+  let location = document.createElement("p");
+  let telf = document.createElement("p");
+  let website = document.createElement("a");
+  let membership = document.createElement("p");
+
+    h2.textContent = company.name;
+    location.textContent = company.location;
+    telf.textContent = company.telf;
+    website.textContent = company.website;
+    membership.textContent = company.membership;
+
+   // build link
+   website.setAttribute('href', company.website)
 
    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-   portrait.setAttribute('src', companie.logourl);
-   portrait.setAttribute('alt', 'Portait of ${companie.name}');
-   portrait.setAttribute('loading','lazy');
+   image.setAttribute('src', company.logourl);
+   image.setAttribute('alt', company.name);
+   image.setAttribute('loading','lazy');
 
    // Add/append the section(card) with the h2 element
-   card.appendChild(portrait);
-   card.appendChild(heading);
-   card.appendChild(paragraph);
-   card.appendChild(paragraph2);
+   card.appendChild(image);
+   card.appendChild(h2);
+   card.appendChild(location);
+   card.appendChild(telf);
+   card.appendChild(membership);
    card.appendChild(website);
-   card.appendChild(paragraph4);
-   
    
 
    // Add/append the existing HTML div with the cards class with the section(card)
